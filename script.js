0b9aa853537d4b0a4deb9f4f5624b89911e8c1b1
BC <46092015+TheRealMrBC@users.noreply.github.com>
2025-08-04 21:04:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -66,16 +66,16 @@ document.addEventListener("DOMContentLoaded", () => {
   
         const specsList = document.createElement("ul");
         specsList.innerHTML = `
-          <li>Maximum dimensions: ${door.Maximum-dimensions}</li>
-          <li>Minimum dimensions: ${door.Minimum-dimensions}</li>
-          <li>Opening speed: ${door.Opening-speed}</li>
-          <li>Closing speed: ${door.Closing-speed}</li>
+          <li>Maximum-dimensions: ${door.Maximum-dimensions}</li>
+          <li>Minimum-dimensions: ${door.Minimum-dimensions}</li>
+          <li>Opening-speed: ${door.Opening-speed}</li>
+          <li>Closing-speed: ${door.Closing-speed}</li>
           <li>Usage: ${door.Usage}</li>
           <li>Technology: ${door.Technology}</li>
           <li>Drum: ${door.Drum}</li>
           <li>Web-Site: ${door.Web-Site}</li>
-          <li>Technical datasheet: ${door.Technical-datasheet}</li>
-          <li>Product Datasheet: ${door.Product-Datasheet}</li>
+          <li>Technical-datasheet: ${door.Technical-datasheet}</li>
+          <li>Product-Datasheet: ${door.Product-Datasheet}</li>
         `;
         specsDiv.appendChild(specsList);
   
